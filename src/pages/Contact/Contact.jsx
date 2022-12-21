f8f3ce94b3bc89b33f2ab1cabb8cb1de79450489
DanhBui <bxdanh1998@gmail.com>
2022-12-21 20:30:45 +0700

    #16. Xây dựng contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,26 @@
+import BannerPage from '../../components/BannerPage';
+import BannerContact from '../../assets/images/banner-pricing.jpg';
+import Button from '../../components/Button';
+
+import './Contact.scss';
 function Contact() {
     return (
         <div className="contact">
-            <div className="container">Contact</div>
+            <BannerPage name="Contact Us" title="Get Helps & Friendly Support" img={BannerContact} />
+            <div className="contact-main">
+                <div className="container">
+                    <form className="contact-form">
+                        <h4>Fillup The Form</h4>
+                        <div className="contact-input">
+                            <input type="text" placeholder="Name" />
+                            <input type="text" placeholder="Email" />
+                            <input type="text" placeholder="Subject" />
+                            <textarea cols="30" rows="10"></textarea>
+                        </div>
+                        <Button className="btn-submit" title="Submit Request" />
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
