47397b6b8af4f27834538c38f3da68e1084c4ded
DanhBui <bxdanh1998@gmail.com>
2022-12-21 16:11:18 +0700

    #14. Xây dựng blog page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,7 +1,17 @@
+import BannerPage from '../../components/BannerPage';
+import RecentCard from '../../components/Recent/RecentCard';
+import BannerBlog from '../../assets/images/banner-about.jpg';
+import './Blog.scss';
+
 function Blog() {
     return (
         <div className="blog">
-            <div className="container">Blog</div>
+            <BannerPage name="Blog" title="Blog Grid - Our Blogs" img={BannerBlog} />
+            <div className="blog-main">
+                <div className=" recent container">
+                    <RecentCard />
+                </div>
+            </div>
         </div>
     );
 }
