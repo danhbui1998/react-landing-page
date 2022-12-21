94e490656dab10af7fcc29a071ef1a982a9d7b4e
DanhBui <bxdanh1998@gmail.com>
2022-12-21 16:20:11 +0700

    #15. Xây dựng pricing page

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -1,7 +1,21 @@
+import BannerPage from '../../components/BannerPage';
+import PriceCard from '../../components/Price/PriceCard';
+import BannerPricing from '../../assets/images/banner-pricing.jpg';
+import './Pricing.scss';
+
 function Pricing() {
     return (
         <div className="pricing">
-            <div className="container">Pricing</div>
+            <BannerPage
+                name="30 days money back guarantee"
+                title="No Extra Fees. Friendly Support"
+                img={BannerPricing}
+            />
+            <div className="pricing-main">
+                <div className=" price container">
+                    <PriceCard />
+                </div>
+            </div>
         </div>
     );
 }
