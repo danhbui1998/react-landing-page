9e085128424c4fd0428120df23d54d681152eb85
DanhBui <bxdanh1998@gmail.com>
2022-12-21 16:00:29 +0700

    #13. Xây dựng services page

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,7 +1,17 @@
+import BannerPage from '../../components/BannerPage';
+import FeaturedCard from '../../components/Featured/FeaturedCard';
+import BannerService from '../../assets/images/banner-services.jpg';
+import './Services.scss';
+
 function Services() {
     return (
         <div className="services">
-            <div className="container">Services</div>
+            <BannerPage name="Services" title="Services -All Services" img={BannerService} />
+            <div className="services-main">
+                <div className=" featured container">
+                    <FeaturedCard />
+                </div>
+            </div>
         </div>
     );
 }
